17e0727c2caa2175fdcd31a982375190fbd33a89
Arvalinno <arvalinno@shopee.com>
2024-05-07 18:53:59 +0700

    add Entry sql and unit test

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,48 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/arvalinno/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T, account Account) Entry {
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	}
+
+	newEntry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, newEntry)
+
+	require.Equal(t, newEntry.AccountID, arg.AccountID)
+	require.Equal(t, newEntry.Amount, arg.Amount)
+
+	require.NotZero(t, newEntry.ID)
+	require.NotZero(t, newEntry.CreatedAt)
+
+	return newEntry
+}
+
+func TestCreateEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	createRandomEntry(t, account)
+}
+
+func TestGetEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	entry1 := createRandomEntry(t, account)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt.Time, entry2.CreatedAt.Time, time.Second)
+}
