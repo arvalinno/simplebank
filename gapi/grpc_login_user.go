1b4489939905c3e4d7820c1d6da9ed95858ec292
Arvalinno <arvalinno@gmail.com>
2025-02-25 17:30:38 +0700

    validation for human/machine friendly response

--- gapi/grpc_login_user.go
+++ gapi/grpc_login_user.go
@@ -6,14 +6,21 @@ import (
 	db "github.com/arvalinno/simplebank/db/sqlc"
 	"github.com/arvalinno/simplebank/pb"
 	"github.com/arvalinno/simplebank/util"
+	"github.com/arvalinno/simplebank/val"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgtype"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	violations := validateLoginUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
 	user, err := server.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
 		if err == pgx.ErrNoRows {
@@ -70,4 +77,16 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 	}
 
 	return rsp, nil
+}
+
+func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+
+	return violations
 }
